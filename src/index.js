025edec4a633e35881e7ee142e6be26726d23955
Darlin Ruben Nina C <78616515+Ruben890@users.noreply.github.com>
2023-04-26 14:28:07 -0400

    dependecias

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const app = require('./app.js');
 const sequelize = require('./database/database.js');
-const { swaggerDocs: v1swaggerDocs } = require('./v1/swagger.js')
+
+
 const main = async () => {
 
     try {
         await sequelize.authenticate();
         app.listen(app.get('port'), () => {
-            v1swaggerDocs(app, app.get('port'))
+           
         })
 
     } catch (error) {
